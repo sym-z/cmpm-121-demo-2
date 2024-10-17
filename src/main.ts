926faf1fb51d46e72121ad888f5d30bf0f438ca3
Jack Sims <jdsims@ucsc.edu>
2024-10-17 14:02:42 -0700

    Testing deployment with a small change to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Hello, my name is Jack.";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
