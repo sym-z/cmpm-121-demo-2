6e1c51c14fa207f44deac85df5a3052209ebeb82
Jack Sims <jdsims@ucsc.edu>
2024-10-18 14:46:51 -0700

    Step 1 Complete. Styled canvas and title

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import "./style.css";
 
-const APP_NAME = "Hello, my name is Jack.";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+// Makes the appending process into a loop.
+// Add app's elements as you go.
+const PAGE : HTMLElement[] = [];
+
+// App title
+const title = document.createElement("h1");
+PAGE.push(title);
+
+// Create drawing canvas
+const canvas = document.createElement("canvas");
+const WIDTH : number = 256;
+const HEIGHT : number = 256;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+PAGE.push(canvas)
+
+const APP_NAME = "Jack's Paint App";
+
+title.textContent = APP_NAME;
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+for(const element of PAGE)
+    {
+        app.appendChild(element)
+    }
