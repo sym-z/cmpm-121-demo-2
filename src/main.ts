807a57301903e3c31415a4f2ba01d89d5aeb5f22
Jack Sims <jdsims@ucsc.edu>
2024-10-18 15:10:16 -0700

    Step 2 Complete. Clear button and drawing system with the help of linked example on the slides.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import "./style.css";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 // Makes the appending process into a loop.
-// Add app's elements as you go.
-const PAGE : HTMLElement[] = [];
 
 // App title
 const title = document.createElement("h1");
-PAGE.push(title);
+app.appendChild(title)
 
 // Create drawing canvas
 const canvas = document.createElement("canvas");
@@ -16,14 +14,53 @@ const WIDTH : number = 256;
 const HEIGHT : number = 256;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
-PAGE.push(canvas)
+app.appendChild(canvas)
+const ctx : CanvasRenderingContext2D | null = canvas.getContext("2d");
+
+// Event Listeners
+// Used the linked quant-paint.glitch.me/paint0.html to help
+const cursor = {active: false, x: 0, y: 0};
+
+canvas.addEventListener("mousedown", (e) => 
+{
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+}
+);
+
+canvas.addEventListener("mousemove", (e) => 
+{
+    if(cursor.active && ctx != null)
+        {
+            ctx.beginPath();
+            ctx.moveTo(cursor.x,cursor.y);
+            ctx.lineTo(e.offsetX,e.offsetY);
+            ctx.stroke();
+            cursor.x = e.offsetX;
+            cursor.y = e.offsetY;
+        }
+})
+
+canvas.addEventListener("mouseup", (e) => 
+{
+    cursor.active = false;
+})
+
+// Clear button
+const clear_button = document.createElement("button");
+clear_button.innerHTML = "CLEAR";
+app.appendChild(clear_button)
+clear_button.addEventListener("click", () =>
+{
+    if(ctx != null)
+    {
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+    }
+})
+
 
 const APP_NAME = "Jack's Paint App";
 
 title.textContent = APP_NAME;
 document.title = APP_NAME;
-
-for(const element of PAGE)
-    {
-        app.appendChild(element)
-    }
