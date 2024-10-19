eff2c66a6470919ee00ae89ea20dc19028ba7a77
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-10-19 16:06:37 -0700

    Step 5 complete. Class for the marker line implemented.

--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,31 @@ interface Point {
   x: number;
   y: number;
 }
-const lines: Point[][] = [];
-const redo_lines: Point[][] = [];
-let current_line: Point[] | null = null;
+class Line {
+  points: Point[] = []
+  constructor(start: Point) {
+    this.points.push(start)
+  }
+
+  display(ctx: CanvasRenderingContext2D) {
+    console.log("Displaying Line")
+    if (this.points.length > 1) {
+      ctx.beginPath();
+      const { x, y } = this.points[0];
+      ctx.moveTo(x, y);
+      for (const { x, y } of this.points) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
+}
+// For new object
+const total_lines: Line[] = [];
+
+const total_redo_lines: Line[] = [];
+
+let curr_line: Line | null = null;
 
 // Event Listeners
 // Used the linked quant-paint.glitch.me/paint0.html and paint1.hmtl to help
@@ -33,7 +55,7 @@ const cursor = { active: false, x: 0, y: 0 };
 canvas.addEventListener("drawing_changed", () => {
   if (ctx != null) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (const line of lines) {
+/*    for (const line of lines) {
       if (line.length > 1) {
         ctx.beginPath();
         const { x, y } = line[0];
@@ -43,19 +65,35 @@ canvas.addEventListener("drawing_changed", () => {
         }
         ctx.stroke();
       }
+    }*/
+    for (const line of total_lines) {
+      line.display(ctx)
     }
   }
 });
 canvas.addEventListener("mousedown", (e) => {
-  current_line = [];
-  lines.push(current_line);
-  redo_lines.splice(0, redo_lines.length);
+  //create empty line
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+  curr_line = new Line({ x: cursor.x, y: cursor.y });
+
+  //push into line array
+  total_lines.push(curr_line)
+
+  //empty redo's
+  total_redo_lines.splice(0, total_redo_lines.length);
+
   cursor.active = true;
-  addLine(e);
+
+  canvas.dispatchEvent(drawing_changed);
 });
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active && ctx != null) {
-    addLine(e);
+      cursor.x = e.offsetX;
+      cursor.y = e.offsetY;
+      curr_line?.points.push({ x: cursor.x, y: cursor.y });
+
+      canvas.dispatchEvent(drawing_changed);
   }
 });
 canvas.addEventListener("mouseup", () => {
@@ -72,8 +110,8 @@ clear_button.innerHTML = "CLEAR";
 app.appendChild(clear_button);
 clear_button.addEventListener("click", () => {
   clearDrawing();
-  lines.splice(0, lines.length);
-  current_line = null;
+  total_lines.splice(0, total_lines.length);
+  curr_line = null;
 });
 
 // Redo button
@@ -81,10 +119,10 @@ const redo_button = document.createElement("button");
 redo_button.innerHTML = "REDO";
 app.appendChild(redo_button);
 redo_button.addEventListener("click", () => {
-  if (redo_lines.length > 0) {
-    const new_line: Point[] | undefined = redo_lines.pop();
+  if (total_redo_lines.length > 0) {
+    const new_line: Line | undefined = total_redo_lines.pop();
     if (new_line != undefined) {
-      lines.push(new_line);
+      total_lines.push(new_line);
       canvas.dispatchEvent(drawing_changed);
     }
   }
@@ -95,12 +133,10 @@ const undo_button = document.createElement("button");
 undo_button.innerHTML = "UNDO";
 app.appendChild(undo_button);
 undo_button.addEventListener("click", () => {
-  if (lines.length > 0) {
-    const old_line: Point[] | undefined = lines.pop();
+  if (total_lines.length > 0) {
+    const old_line: Line | undefined = total_lines.pop();
     if (old_line != undefined) {
-      redo_lines.push(old_line);
-      console.log(redo_lines)
-      console.log("lines: ", lines)
+      total_redo_lines.push(old_line);
       canvas.dispatchEvent(drawing_changed);
     }
   }
@@ -113,14 +149,5 @@ document.title = APP_NAME;
 function clearDrawing() {
   if (ctx != null) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-}
-
-function addLine(e: MouseEvent) {
-  if (current_line != null) {
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
-    current_line.push({ x: cursor.x, y: cursor.y });
-    canvas.dispatchEvent(drawing_changed);
   }
 }
